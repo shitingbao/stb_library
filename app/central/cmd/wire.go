1bbebbddcdb29f660433cf84c096280d8bbdd597
shitingbao <871258317@qq.com>
2021-10-21 18:54:35 +0800

    library start

---
+++ app/central/cmd/wire.go
@@ -0,0 +1,22 @@
+//go:build wireinject
+// +build wireinject
+
+// The build tag makes sure the stub is not built in the final build.
+
+package main
+
+import (
+	"github.com/go-kratos/kratos/v2"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/google/wire"
+	"stb-library/app/central/internal/biz"
+	"stb-library/app/central/internal/conf"
+	"stb-library/app/central/internal/data"
+	"stb-library/app/central/internal/server"
+	"stb-library/app/central/internal/service"
+)
+
+// initApp init kratos application.
+func initApp(*conf.Server, *conf.Data, log.Logger) (*kratos.App, func(), error) {
+	panic(wire.Build(server.ProviderSet, data.ProviderSet, biz.ProviderSet, service.ProviderSet, newApp))
+}
