fb450d16b295a4b3d2a2dbdfad47a8bf1b68a82e
shitingbao <871258317@qq.com>
2021-10-22 14:21:10 +0800

    add storage

---
+++ app/storage/cmd/wire.go
@@ -0,0 +1,22 @@
+//go:build wireinject
+// +build wireinject
+
+// The build tag makes sure the stub is not built in the final build.
+
+package main
+
+import (
+	"github.com/go-kratos/kratos/v2"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/google/wire"
+	"stb-library/app/storage/internal/biz"
+	"stb-library/app/storage/internal/conf"
+	"stb-library/app/storage/internal/data"
+	"stb-library/app/storage/internal/server"
+	"stb-library/app/storage/internal/service"
+)
+
+// initApp init kratos application.
+func initApp(*conf.Server, *conf.Data, log.Logger) (*kratos.App, func(), error) {
+	panic(wire.Build(server.ProviderSet, data.ProviderSet, biz.ProviderSet, service.ProviderSet, newApp))
+}
