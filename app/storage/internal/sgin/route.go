9500ebba89cc45edeeda7c675fff696444c7fcc4
shitingbao <871258317@qq.com>
2022-02-14 15:47:53 +0800

    perf: *

--- app/storage/internal/sgin/route.go
+++ app/storage/internal/sgin/route.go
@@ -75,7 +75,8 @@ func (s *Sgin) sayHello(ctx *gin.Context) {
 }
 
 func cross(c *gin.Context) {
-	c.Header("Access-Control-Allow-Origin", "http://127.0.0.1,http://124.70.156.31,http://socket1.cn")
+	c.Header("Access-Control-Allow-Origin", "*")
+	// c.Header("Access-Control-Allow-Origin", "http://127.0.0.1,http://124.70.156.31,http://socket1.cn")
 	c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization")
 	c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
 	c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
