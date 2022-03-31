91e900c80867d09d7a3f039df740cc4e3bd446db
shitingbao <871258317@qq.com>
2022-03-31 15:57:02 +0800

    perf: add assets

--- app/storage/internal/sgin/route.go
+++ app/storage/internal/sgin/route.go
@@ -1,8 +1,13 @@
 package sgin
 
 import (
+	"log"
+	"mime"
+	"net/url"
+	"path/filepath"
 	"stb-library/app/storage/internal/biz"
 	"stb-library/lib/response"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,6 +15,12 @@ import (
 var (
 	tokenKey = "Authorization"
 )
+
+// 自己解析静态资源
+func init() {
+	mime.AddExtensionType(".js", "text/javascript")
+	mime.AddExtensionType(".css", "text/css; charset=utf-8")
+}
 
 func cross(ctx *gin.Context) {
 	// ctx.Header("Access-Control-Allow-Origin", "*")
@@ -34,6 +45,7 @@ func (s *Sgin) verification(ctx *gin.Context) {
 
 func (s *Sgin) setRoute() {
 	s.g.Use(cross)
+	s.g.Use(s.assets)
 	rg := s.g.Group("/api")
 	{
 		rg.POST("/login", s.login)
@@ -76,4 +88,49 @@ func (s *Sgin) health(ctx *gin.Context) {
 		return
 	}
 	response.JsonOK(ctx, n)
+}
+
+// assets 静态资源反馈
+func (s *Sgin) assets(ctx *gin.Context) {
+	log.Println("URL=====:", ctx.Request.URL.String())
+	//反馈静态主页，需要下面css，和js以及fonts的资源路径配合
+	//这里的第二个参数就是你要反馈的资源地址，也就是类似index.html的完整路径
+	switch ctx.Request.URL.String() {
+	case "/":
+		s.g.StaticFile("/opt/nginx", filepath.Join("dist", ctx.Request.URL.String(), "index.html"))
+		return
+	case "/_app.config.js", "favicon.ico":
+		s.g.StaticFile("/opt/nginx", filepath.Join("dist", ctx.Request.URL.String()))
+		return
+	}
+
+	paths, err := parsePaths(ctx.Request.URL)
+	log.Println("parsePaths======:", paths, err)
+	//这里的path反馈工作元素内容待定
+	if err != nil {
+		// w.WriteHeader(http.StatusBadRequest)
+		// w.Write(nil)
+		return
+	}
+
+	if paths[0] == "assets" || paths[0] == "resource" {
+		s.g.StaticFile("/opt/nginx", filepath.Join("dist", ctx.Request.URL.String()))
+		// http.ServeFile(w, r, filepath.Join(str, "dist", paths[0], paths[len(paths)-1]))
+		return
+	}
+
+}
+
+//解析url
+func parsePaths(u *url.URL) ([]string, error) {
+	paths := []string{}
+	pstr := u.EscapedPath()
+	for _, str := range strings.Split(pstr, "/")[1:] {
+		s, err := url.PathUnescape(str)
+		if err != nil {
+			return nil, err
+		}
+		paths = append(paths, s)
+	}
+	return paths, nil
 }
