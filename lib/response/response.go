181dd7dab937f5af52395e85c82cad8f945ef828
shitingbao <871258317@qq.com>
2022-04-01 16:49:50 +0800

    fix: origin

--- lib/response/response.go
+++ lib/response/response.go
@@ -14,12 +14,6 @@ const (
 
 func JsonOK(ctx *gin.Context, data ...interface{}) {
 
-	ctx.Header("Access-Control-Allow-Origin", "http://socket1.cn:8000")
-	ctx.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization")
-	ctx.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
-	ctx.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
-	ctx.Header("Access-Control-Allow-Credentials", "true")
-
 	var res interface{}
 	if len(data) > 0 {
 		res = data[0]
@@ -32,12 +26,6 @@ func JsonOK(ctx *gin.Context, data ...interface{}) {
 }
 
 func JsonErr(ctx *gin.Context, err error, data interface{}) {
-
-	ctx.Header("Access-Control-Allow-Origin", "http://socket1.cn:8000")
-	ctx.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization")
-	ctx.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
-	ctx.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
-	ctx.Header("Access-Control-Allow-Credentials", "true")
 
 	if err == nil {
 		err = errors.New("")
