70f199befdbe509940ed62601150fcb08e525ce2
shitingbao <871258317@qq.com>
2024-03-22 16:28:51 +0800

    feat: add python database

--- app/py/database.py
+++ app/py/database.py
@@ -1,3 +1,36 @@
-import psycopg2
+import mysql.connector
 
-con = psycopg2.connect("")
+# db = mysql.connector.connect(
+#     user="root", password="4116bbDD#", host="47.99.104.79", database="mengma"
+# )
+
+
+def getMysqlDb(user, password, host, database):
+    # 打开数据库连接
+    db = mysql.connector.connect(
+        user=user, password=password, host=host, database=database
+    )
+    return db
+
+
+def ListTsk():
+    db = getMysqlDb(
+        user="root", password="4116bbDD#", host="47.99.104.79", database="mengma"
+    )
+
+    # 使用cursor()方法获取操作游标
+    cursor = db.cursor()
+
+    # 使用execute方法执行SQL语句
+    cursor.execute("SELECT * from company_task limit 3")
+
+    # 使用 fetchone() 方法获取一条数据
+    data = cursor.fetchall()
+
+    print("data:", data)
+
+    # 关闭数据库连接
+    db.close()
+
+
+ListTsk()
